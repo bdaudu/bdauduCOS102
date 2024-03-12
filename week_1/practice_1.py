e4d7911d7fe481c6c47d2a1440a89354565c512f
b.dauduCOS101 <basil.daudu@pau.edu.ng>
2024-03-12 16:47:38 +0100

    (Week 1 Commit)

---
+++ week_1/practice_1.py
@@ -0,0 +1,12 @@
+
+num = 100
+print(num)
+print("Data Type of variable num is", type(num))
+
+fnum = 34.45
+print(fnum)
+print("Data Type of variable fnum is", type(fnum))
+
+cnum = 3 + 4j
+print(cnum)
+print("Data Type of variable cnum is", type(cnum))
---
+++ week_1/practice_2.py
@@ -0,0 +1,8 @@
+str1 = "Hi. My name is Mathew. I am String"
+str2 = "Hi. My name is Precious. I am also a String"
+
+print(str1)
+print(type(str1))
+
+print(str2)
+print(type(str2))
