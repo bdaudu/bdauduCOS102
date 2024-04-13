c3f47f83ee4b891cb04bf1a77982d7d1c2571153
bdaudu <147815184+bdaudu@users.noreply.github.com>
2024-04-13 04:45:18 +0100

    Delete week_5 directory

--- week_5/practice_1.py
+++
@@ -1,6 +0,0 @@
-def printme(str):
-    print(str)
-    return
-
-printme("I'm first call to user defined function!")
-printme("Again second call to the samer function")
--- week_5/practice_5.py
+++
@@ -1,6 +0,0 @@
-def printinfo(name,age):
-    print("Name: ", name)
-    print("Age: ", age)
-    return
-
-printinfo(age=50, name="miki")
